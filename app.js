01ca327304b4a3bface92365efaca31fd242a525
Victor Duran <victor.duran.em@gmail.com>
2022-10-10 21:34:19 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  sendSMS()
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+function sendSMS() {
+  require('dotenv').config()
+
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const fromPhone = process.env.FROM_PHONE
+  const toPhone = process.env.TO_PHONE
+
+  const client = require('twilio')(accountSid, authToken);
+
+  client.messages
+    .create({
+      body: 'Hello there',
+      from: fromPhone,
+      to: toPhone
+    })
+    .then(message => console.log(message.sid));
+}
