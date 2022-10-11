4cec6350541f1b9ec053657c82b0dc792b790b20
Victor Duran <victor.duran.em@gmail.com>
2022-10-10 21:44:49 -0500

    Move require to the top of the file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -13,7 +15,6 @@ app.listen(port, () => {
 
 
 function sendSMS() {
-  require('dotenv').config()
 
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
